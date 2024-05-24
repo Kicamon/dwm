91f96816c8bd4dfad8674eda96fe787e7b406695
KicamonIce <1532931259@qq.com>
2024-05-24 20:18:07 +0800

    2024/05/24,20:18

--- dwm.c
+++ dwm.c
@@ -1662,7 +1662,7 @@ void incnmaster(const Arg *arg) {
     int nmaster = selmon->nmaster + arg->i;
     if (selmon->bt <= 1)
         nmaster = 1;
-    else if (nmaster >= 3)
+    else if (nmaster > 3)
         nmaster = 1;
     selmon->nmaster = selmon->pertag->nmasters[selmon->pertag->curtag] = MAX(nmaster, 1);
     arrange(selmon);
