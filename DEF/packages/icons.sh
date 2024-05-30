d285769de2cafa8d7bec6b528a8987bd6153054f
Kicamon <1532931259@qq.com>
2024-05-30 09:37:24 +0800

    2024/05/30,09:37

--- DEF/packages/icons.sh
+++ DEF/packages/icons.sh
@@ -8,18 +8,23 @@ color="^c#2D1B46^^b#5555660x66^"
 signal=$(echo "^s$this^" | sed 's/_//')
 
 with_v2raya() {
-  [ "$(ps aux | grep -v grep | grep 'v2raya')" ] && icons=(${icons[@]} "󰀝")
+  [ "$(ps aux | grep -v grep | grep 'v2raya')" ] && icons=(${icons[@]} "")
+}
+
+with_hot() {
+  [ "$(nmcli dev | grep 'ap0')" ] && icons=(${icons[@]} "󱜠")
 }
 
 with_bluetooth() {
   # 此处为自用蓝牙设备的 MAC 地址，你可以自定义该部分
   [ ! "$(command -v bluetoothctl)" ] && echo command not found: bluetoothctl && return
-  [ "$(bluetoothctl info 88:C9:E8:14:2A:72 | grep 'Connected: yes')" ] && icons=(${icons[@]} "")
+  [ "$(bluetoothctl info 6C:D3:EE:3E:CB:5F | grep 'Connected: yes')" ] && icons=(${icons[@]} "󰂯")
 }
 
 update() {
-  icons=("")
+  icons=("󰍜")
   with_v2raya
+  with_hot
   with_bluetooth
 
   text=" ${icons[@]} "
@@ -30,8 +35,9 @@ update() {
 
 notify() {
   texts=""
-  [ "$(ps aux | grep -v grep | grep 'v2raya')" ] && texts="$texts\n v2raya 已启动"
-  [ "$(bluetoothctl info 88:C9:E8:14:2A:72 | grep 'Connected: yes')" ] && texts="$texts\n WH-1000XM4 已链接"
+  [ "$(ps aux | grep -v grep | grep 'v2raya')" ] && texts="$texts\n v2raya 已启动"
+  [ "$(bluetoothctl info 6C:D3:EE:3E:CB:5F | grep 'Connected: yes')" ] && texts="$texts\n󰂯 蓝牙耳机 已链接"
+  [ "$(nmcli dev | grep 'ap0')" ] && texts="$texts\n󱜠 热点 已开启"
   [ "$texts" != "" ] && notify-send " Info" "$texts" -r 9527
 }
 
